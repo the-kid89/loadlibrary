d3a1dacad9d942037e8a7dbab94eb12022eb7ddf
Emett Speer <espeer@fortinet.com>
2018-02-07 15:43:04 -0800

    Converted output to standard out

--- mpclient.c
+++ mpclient.c
@@ -64,29 +64,29 @@ DWORD (* __rsignal)(PHANDLE KernelHandle, DWORD Code, PVOID Params, DWORD Size);
 static DWORD EngineScanCallback(PSCANSTRUCT Scan)
 {
     if (Scan->Flags & SCAN_MEMBERNAME) {
-        LogMessage("Scanning archive member %s", Scan->VirusName);
+        printf("Scanning archive member %s", Scan->VirusName);
     }
     if (Scan->Flags & SCAN_FILENAME) {
-        LogMessage("Scanning %s", Scan->FileName);
+        printf("Scanning %s", Scan->FileName);
     }
     if (Scan->Flags & SCAN_PACKERSTART) {
-        LogMessage("Packer %s identified.", Scan->VirusName);
+        printf("Packer %s identified.", Scan->VirusName);
     }
     if (Scan->Flags & SCAN_ENCRYPTED) {
-        LogMessage("File is encrypted.");
+        printf("File is encrypted.");
     }
     if (Scan->Flags & SCAN_CORRUPT) {
-        LogMessage("File may be corrupt.");
+        printf("File may be corrupt.");
     }
     if (Scan->Flags & SCAN_FILETYPE) {
-        LogMessage("File %s is identified as %s", Scan->FileName, Scan->VirusName);
+        printf("File %s is identified as %s", Scan->FileName, Scan->VirusName);
     }
     if (Scan->Flags & 0x08000022) {
-        LogMessage("Threat %s identified.", Scan->VirusName);
+        printf("Threat %s identified.", Scan->VirusName);
     }
     // This may indicate PUA.
     if ((Scan->Flags & 0x40010000) == 0x40010000) {
-        LogMessage("Threat %s identified.", Scan->VirusName);
+        printf("Threat %s identified.", Scan->VirusName);
     }
     return 0;
 }
